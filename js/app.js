750c1ec859e6a9cf276048871de4e2d9662aeca7
Juan Martin Marco <jmmarco@gmail.com>
2016-04-05 19:21:05 -0300

    Add modifications

--- js/app.js
+++ js/app.js
@@ -1,56 +1,129 @@
-// Get Coordinates
+(function() {
+  var output = $('#out');
 
-function geoFindMe() {
+  if (!navigator.geolocation) {
+    output.html('<h4>Geolocation is not supported by your Browser</h4>');
+    return;
+  }
 
-    var output = $('#out');
+  function success(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    //output.html('<h4>Latitude is: ' + lat + '&deg; <br>Longitude is ' + lon + '&deg;</h4>');
 
-    if (!navigator.geolocation) {
-        output.html('<h4>Geolocation is not supported by your Browser</h4>');
-        return;
-    }
+    var img = new Image();
+    img.src = 'https://maps.googleapis.com/maps/api/staticmap?center=' + lat + ',' + lon + '&zoom=13&size=300x300&sensor=false';
+    output.html(img);
+    //$('img').attr('class', 'center-block');
 
-    function success(position) {
-        var lat = position.coords.latitude;
-        var lon = position.coords.longitude;
-        output.html('<h4>Latitude is: ' + lat + '° <br>Longitude is ' + lon + '°</h4>');
+    var unit = function(value) {
+      if (value == 'metric')
+        return value;
+      else if (value == 'farenheit')
+        return value;
+    };
 
-        var img = new Image();
-        img.src = 'https://maps.googleapis.com/maps/api/staticmap?center=' + lat + ',' + lon + '&zoom=8&size=300x300&sensor=false';
-        output.append(img);
 
-        var config = {
-            apiKey : '0e9a952df087f4f3582c374303cf7e7e',
-        };
+    var config = {
+      apiKey: '0e9a952df087f4f3582c374303cf7e7e',
+      units: '&units=' + unit
+    };
 
-        // AJAX Request
-        var weatherUrl = 'api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=' + config.apiKey;
-        $.ajax({
+    // AJAX Request
+    var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=' + config.apiKey;
+    console.log(weatherUrl);
+    $.ajax({
 
-            url: weatherUrl,
+      url: weatherUrl,
+      dataType: 'jsonp',
 
-            beforeSend: function() {
-                // this is where we append a loading image
-                $('#ajax-panel').html('<div class="loading"><img src="images/loading.gif" alt="Loading..." /></div>');
-            },
-            success: function(data) {
-                // successful request; do something with the data
-                $('#ajax-panel').empty();
-                $(data).find('item').each(function(i) {
-                    $('#ajax-panel').append('<h4>' + $(this).find('title').text() + '</h4><p>' + $(this).find('link').text() + '</p>');
-                });
-            },
-            error: function() {
-                // failed request; give feedback to user
-                $('#ajax-panel').html('<p class="error"><strong>Oops!</strong> Try that again in a few moments.</p>');
-            }
+      // Display a loading wheel while data is fetched
+      beforeSend: function() {
+        $('#ajax-panel').html('<div class="loading"><img src="images/loading_bullets.gif" alt="Loading..." /></div>');
+      },
+      // On success do stuff
+      success: function(data) {
+
+        // Credit: https://gist.github.com/tbranyen/62d974681dea8ee0caa1
+
+        /*console.log(weatherIcons);
+        req = $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=' + config.apiKey + '&callback=?');
+        req.then(function(data) { */
+          var prefix = 'wi wi-';
+          var code = data.weather[0].id;
+          console.log(code);
+          var icon = weatherIcons[code].icon;
+
+          // If we are not in the ranges mentioned above, add a day/night prefix.
+          if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+            icon = 'day-' + icon;
+          }
+
+          // Finally tack on the prefix.
+          icon = prefix + icon;
+
+
+        console.log(data.weather);
+        //var icon = '<img id="icon" src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png" alt="Weather icon" width="80">';
+        var wIcon = '<i class="' + icon + '"></i>';
+        var location = "<p>It looks like you're located in: " + data.name + ', ' + data.sys.country + '</p>';
+        var celsius = Math.round(data.main.temp - 273.15);
+        var farenheit = Math.round(9 / 5 * (data.main.temp - 273.15) + 32);
+
+        var temp = '<p class="temp">' + '<span class="active">' + celsius + '&deg;' + 'C' + '</span>' + ' | ' + '<span class="inactive">F</span>' + '</p>';
+
+
+        $('#ajax-panel').empty();
+        $('#ajax-panel').append(location);
+        $('#ajax-panel').append(wIcon);
+        $('#ajax-panel').append(temp);
+        //$('.wi').addClass('');
+        $('.temp').attr('class', 'text-center');
+        $('p').addClass('location');
+
+        /*$('#switch').on('click', function() {
+            $('#ajax-panel').find('span').remove();
+            $('#ajax-panel').append(farenheit);
+        });*/
+
+
+        $('.inactive').hover(function() {
+          console.log(this);
+          $(this).addClass('highlight');
+        }, function() {
+          $(this).removeClass('highlight');
         });
-    } // End of success
 
-    function error() {
-        output.html('Unable to retrieve your location');
-    }
 
-    output.html('<h4>Locating...</h4>');
+        $(document).on('click', '.temp', function() {
+          console.log(this);
+          $(this).remove();
+          console.log(this);
 
-    navigator.geolocation.getCurrentPosition(success, error);
-}
+        });
+
+
+
+        $(data).find('item').each(function(i) {
+          console.log(this);
+          $('#ajax-panel').append('<h4>' + $(this).find('title').text() + '</h4><p>' + $(this).find('link').text() + '</p>');
+        });
+      },
+      // If something went wrong..
+      error: function() {
+        $('#ajax-panel').html('<p class="error"><strong>Oops!</strong> Try that again in a few moments.</p>');
+      }
+    });
+  } // End of success
+
+  function error() {
+    output.html('Unable to retrieve your location');
+  }
+
+  output.html('<h4>Locating...</h4>');
+  output.append('<div class="loading"><img src="images/loading_bullets.gif" alt="Loading..." /></div>');
+
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+})();
