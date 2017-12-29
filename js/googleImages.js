1898e07b7a3a4a44b58af5a1437013fb5a803a01
Juan Martin Marco <jmmarco@gmail.com>
2017-12-28 21:19:49 -0300

    Add google maps image generator code

---
+++ js/googleImages.js
@@ -0,0 +1,13 @@
+var img = new Image();
+img.src =
+  "https://maps.googleapis.com/maps/api/staticmap?center=" +
+  lat +
+  "," +
+  lon +
+  "&zoom=13&size=300x300&sensor=false";
+
+
+var body = documement.get
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(img)
